937c0425f7c91f6b1bcbbf2a2fc58d928a82649c
Negipallavi <149373681+Negipallavi@users.noreply.github.com>
2023-12-01 19:03:23 +0530

    Add files via upload

---
+++ sumanddifferenceoftwonumber.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+int a,b,sum,difference;	
+    scanf("%d %d ",&a,&b);
+    sum=a+b;
+    difference=a-b;
+    printf("%d %d\n",sum,difference);
+float x,y,s,d;       
+scanf("%f%f ",&x,&y);
+    s=x+y;
+    d=x-y;
+printf("%0.1f %0.1f ",s,d);
+    
+ 
+    return 0;
+}
